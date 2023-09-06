0765b3effe35f872e8ad27770fde99bc6eb135b3
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-06 16:05:55 +0300

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    var stringBuilder = new System.Text.StringBuilder("<table>");
+
+    foreach (var header in context.Request.Headers)
+    {
+        stringBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+    }
+
+    stringBuilder.Append("</table>");
+    await context.Response.WriteAsync(stringBuilder.ToString());
+});
+
+
+app.Run();
