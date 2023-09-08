2f205f436190b81125d8b37091f46ad6a5a4e050
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-08 13:22:32 +0300

    done

---
+++ WebApplication1/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1
+{
+    public class Company
+    {
+        public string name;
+        public int value;
+        public Company() {
+            name = "test";
+            value = 1;
+        }
+        public Company(string name, int value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,16 +1,15 @@
+using WebApplication1;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+Random rand = new();
+Company company = new("Samsung", 1000);
 app.Run(async (context) =>
 {
     context.Response.ContentType = "text/html; charset=utf-8";
-    var stringBuilder = new System.Text.StringBuilder("<table>");
-
-    foreach (var header in context.Request.Headers)
-    {
-        stringBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
-    }
-
-    stringBuilder.Append("</table>");
+    var stringBuilder = new System.Text.StringBuilder("");
+    stringBuilder.Append($"{company.name} - {company.value}<br>");
+    stringBuilder.Append($"<text>{rand.Next(0, 101)}</text>");
     await context.Response.WriteAsync(stringBuilder.ToString());
 });
 
